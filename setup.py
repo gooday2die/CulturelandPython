93f0e0295b0392e420faf3a261d5adb107f17fa4
gooday2die <edina00@naver.com>
2022-04-08 18:01:33 +0900

    Edited setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+
+with open("README.md", "r", encoding="utf-8") as fh:  # Read README.md file for long_description
+    long_description = fh.read()
+
 setup(
     name='CulturelandPython',
     version='0.1.2',
@@ -9,6 +13,8 @@ setup(
     author_email='edina00@naver.com',
     license='GPL',
     packages=['CulturelandPython'],
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     install_requires=['selenium', 'webdriver-manager'],
     classifiers=[
         'Programming Language :: Python :: 3.8',
