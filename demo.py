f955990fb977b5bff6eb2367672e106ac13fac61
gooday2die <edina00@naver.com>
2022-04-08 17:47:25 +0900

    Edited demo

--- demo.py
+++ demo.py
@@ -1,10 +1,7 @@
 from CulturelandPython import client
-import os
 
 
 if __name__ == "__main__":
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # need to specify the Dir of DB file
-    chrome_driver_path_linux = os.path.join(BASE_DIR, "chromedriver")
-    c = client.CulturelandClient('username', 'passwd', chrome_driver_path_linux)
+    c = client.CulturelandClient('username', 'passwd')
     c.redeem('4180-0115-2657-8251')
     c.disconnect()
